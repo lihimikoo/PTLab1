aa9e03b20d5a246f1640142f05713d5e697c166f
lihimiko <sixseven@gmail.com>
2026-09-04 11:09:10 +0300

    Add JSONDataReader class and tests

---
+++ src/JSONDataReader.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from Types import DataType
+from DataReader import DataReader
+import json
+
+
+class JSONDataReader(DataReader):
+    def __init__(self) -> None:
+        self.data: DataType = {}
+
+    def read(self, path: str) -> DataType:
+        with open(path, encoding='utf-8') as file:
+            json_data = json.load(file)
+        
+        for student_name, subjects in json_data.items():
+            self.data[student_name] = []
+            for subject, score in subjects.items():
+                self.data[student_name].append((subject, int(score)))
+        
+        return self.data
---
+++ test/test_JSONDataReader.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import pytest
+from src.Types import DataType
+from src.JSONDataReader import JSONDataReader
+
+
+class TestJSONDataReader:
+    @pytest.fixture()
+    def json_content_and_data(self) -> tuple[str, DataType]:
+        json_text = '''{
+            "Иванов Иван Иванович": {
+                "математика": 80,
+                "программирование": 90,
+                "литература": 76
+            },
+            "Петров Петр Петрович": {
+                "математика": 100,
+                "социология": 90,
+                "химия": 61
+            }
+        }'''
+        
+        data: DataType = {
+            "Иванов Иван Иванович": [
+                ("математика", 80),
+                ("программирование", 90),
+                ("литература", 76)
+            ],
+            "Петров Петр Петрович": [
+                ("математика", 100),
+                ("социология", 90),
+                ("химия", 61)
+            ]
+        }
+        
+        return json_text, data
+
+    @pytest.fixture()
+    def filepath_and_data(self, json_content_and_data: tuple[str, DataType], tmpdir) -> tuple[str, DataType]:
+        p = tmpdir.mkdir("datadir").join("my_data.json")
+        p.write_text(json_content_and_data[0], encoding='utf-8')
+        return str(p), json_content_and_data[1]
+
+    def test_read(self, filepath_and_data: tuple[str, DataType]) -> None:
+        reader = JSONDataReader()
+        result = reader.read(filepath_and_data[0])
+        assert result == filepath_and_data[1]
+
+    def test_init(self) -> None:
+        reader = JSONDataReader()
+        assert reader.data == {}
